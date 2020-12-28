bcabc4affc0d93b6da2196054cc57e0a2b4d0910
ipoliarush <ip.ua97@gmail.com>
2020-12-29 00:00:59 +0200

    up

--- src/components/auth/AuthRegister.vue
+++ src/components/auth/AuthRegister.vue
@@ -1,6 +1,6 @@
 <template>
   <auth>
-    <form class="form" method="post" action="/signup">
+    <form class="form" method="post" action="/signup" @submit.prevent="signup">
       <div class="form__body">
         <h3 class="form__title">Реєстрація</h3>
         <div class="form__item">
@@ -77,6 +77,7 @@ import Auth from "./Auth";
 import IconBase from "@/components/icons/IconBase";
 import IconFacebookAuth from "@/components/icons/IconFacebookAuth";
 import IconGoogleAuth from "@/components/icons/IconGoogleAuth";
+import { required, minLength, maxLength } from "vuelidate/lib/validators";
 
 export default {
   components: {
@@ -87,10 +88,49 @@ export default {
   },
   name: "AuthRegister",
   props: {},
-  data() {
-    return {};
-  },
+  data: () => ({
+    submitStatus: null,
+    firstName: "",
+    lastName: "",
+    email: "",
+    password: "",
+    formData: new FormData(),
+  }),
   computed: {},
+  methods: {
+    signup() {
+
+    }
+  },
+  validations: {
+    firstName: {
+      required,
+      minLength: minLength(2),
+      maxLength: maxLength(60),
+    },
+    lastName: {
+      required,
+      minLength: minLength(2),
+      maxLength: maxLength(60),
+    },
+    email: {
+      required,
+      email(email) {
+        return /^(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])$/.test(
+          email
+        );
+      },
+      minLength: minLength(2),
+      maxLength: maxLength(100),
+    },
+    password: {
+      required,
+      minLength: minLength(6),
+      password(password) {
+        return /[a-zA-Z0-9]/.test(password);
+      },
+    },
+  },
 };
 </script>
 
--- src/main.js
+++ src/main.js
@@ -2,9 +2,13 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router/router'
 import store from './store'
+import Vuelidate from 'vuelidate'
+
 import './assets/styles/_reset.scss'
 
 Vue.config.productionTip = false
+
+Vue.use(Vuelidate)
 
 new Vue({
   render: h => h(App),
