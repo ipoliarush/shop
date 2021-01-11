5cf8b4a0543b6551e5462b09cb42743b020bebc4
Ivan Poliarush <56694325+ipoliarush@users.noreply.github.com>
2021-01-11 09:03:28 +0200

    mobile menu

--- src/store/mutations.js
+++ src/store/mutations.js
@@ -1,6 +1,7 @@
 export default {
   RESIZE (state) {
     if (window.innerWidth < 992) state.isOpen = false
+    else state.isMobileMenu = false
   },
   REVERSE (state) {
     state.isOpen = !state.isOpen
